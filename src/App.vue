4fef988bbb6d55fbf6339a7b3200e8293d70e2a0
unknown <novinkinaa.i20@gmail.com>
2021-12-14 21:50:13 +0300

    2.8

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
               KIPU
             </v-list-item-title>
             <v-list-item-subtitle>
-              Project
+              Vue project
             </v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
@@ -17,6 +17,7 @@
           <v-list-item
           v-for="link in links"
           :key="link.title"
+          :to="link.url"
           >
             <v-list-item-icon>
               <v-icon>{{ link.icon }}</v-icon>
@@ -35,6 +36,8 @@
        <v-btn 
        v-for="link in links"
        :key="link.title" 
+       :key="link.title"
+       :to="link.url" 
        text><v-icon left>{{ link.icon }}</v-icon>{{ link.title }}</v-btn>
     </v-toolbar-items>
   </v-app-bar>
@@ -43,7 +46,6 @@
   </v-main>
   </v-app>
 </template>
-
 <script>
 export default {
   data() {
