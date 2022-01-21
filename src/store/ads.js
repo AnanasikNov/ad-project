dbff69f1880f5cd3fd76577c70c8953e6341578e
unknown <novinkinaa.i20@gmail.com>
2022-01-21 16:41:03 +0300

    РЕДАКТИРОВАНИЕ ОБЪЯВЛЕНИЯ

--- src/store/ads.js
+++ src/store/ads.js
@@ -50,8 +50,14 @@ export default {
         },
         loadAds (state, payload) {
             state.ads = payload
-        }
-
+        },
+        updateAd (state, {title, desc, id}) {
+          const ad = state.ads.find(a => {
+            return a.id === id
+          })
+          ad.title = title
+          ad.desc = desc
+        }  
     },
     actions: {
         createAd({commit},payload){
@@ -129,6 +135,19 @@ export default {
                 throw error
             }
         }
+    },
+    async updateAd ({commit},{title,desc,id}) {
+      commit('clearError')
+        commit('setLoading', true)
+        try {
+          await fb.database().ref("ads").child(id).update({ title, desc })
+          commit('updateAd',{ title, desc, id})
+          commit('setLoading', false)
+        } catch (error) {
+          commit('setError', error.message)
+          commit('setLoading', false)
+          throw error
+        }
     },   
 	getters: {
 		ads(state) {
@@ -146,6 +165,8 @@ export default {
             return id => {
             return state.ads.find(ad => ad.id == id)
             }
-        }        
-}
+         },
+     
+     
+       }
 }
